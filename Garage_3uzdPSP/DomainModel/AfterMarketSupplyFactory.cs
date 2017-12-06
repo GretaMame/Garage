a6731124474b4e5b49d88eaac7880faf8f025b20
Greta Mameniskyte <gretuka27@gmail.com>
2017-12-06 19:41:44 +0200

    name fixed

---
+++ Garage_3uzdPSP/DomainModel/AfterMarketSupplyFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_3uzdPSP.DomainModel
+{
+    class AfterMarketSupplyFactory : ISupplyFactory
+    {
+        public IPart createPart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISupplier createSupplier()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
