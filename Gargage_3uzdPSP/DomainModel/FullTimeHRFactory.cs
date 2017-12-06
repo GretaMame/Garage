32a9dd3fd9d9b996f9c41cc1ee14f374dd094853
Greta Mameniskyte <gretuka27@gmail.com>
2017-12-06 19:21:43 +0200

    added classes and interfaces

---
+++ Gargage_3uzdPSP/DomainModel/FullTimeHRFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gargage_3uzdPSP.DomainModel
+{
+    class FullTimeHRFactory : IHRFactory
+    {
+        public IEmployee createEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISalary createSalary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gargage_3uzdPSP/DomainModel/IHRFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gargage_3uzdPSP.DomainModel
+{
+    interface IHRFactory
+    {
+        IEmployee createEmployee();
+        ISalary createSalary();
+    }
+}
